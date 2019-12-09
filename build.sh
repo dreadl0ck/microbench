cc59cd746b3f7330fc12a9a6cbc6419e2a7f6213
dreadl0ck <dreadl0ck@protonmail.ch>
2019-12-09 16:34:34 +0100

    name binary qemubench

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 if [[ "$1" == "qemu" ]]; then
   echo "building firebench tool with QEMU support"
-  GOOS=linux go build -ldflags="-X main.EngineType=qemu" -o bin/firebench -i github.com/dreadl0ck/firebench/cmd
+  GOOS=linux go build -ldflags="-X main.EngineType=qemu" -o bin/qemubench -i github.com/dreadl0ck/firebench/cmd
 else
   echo "building firebench tool with Firecracker support"
   GOOS=linux go build -ldflags="-X main.EngineType=fc" -o bin/firebench -i github.com/dreadl0ck/firebench/cmd
