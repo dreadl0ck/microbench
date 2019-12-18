706616b78610fdb7c9bb6910e50a4709eace1156
Philippe Partarrieu <ppartarr@outlook.com>
2019-12-18 11:55:53 +0100

    Make build script compile qemubench and firebench

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if [[ "$1" == "qemu" ]]; then
-  echo "building qemubench tool with QEMU support"
-  GOOS=linux go build -ldflags="-X main.EngineType=qemu" -o bin/qemubench -i github.com/dreadl0ck/firebench/cmd
-else
-  echo "building firebench tool with Firecracker support"
-  GOOS=linux go build -ldflags="-X main.EngineType=fc" -o bin/firebench -i github.com/dreadl0ck/firebench/cmd
-fi
+set -e
+
+echo "building qemubench tool with QEMU support"
+GOOS=linux go build -ldflags="-X main.EngineType=qemu" -o bin/qemubench -i github.com/dreadl0ck/firebench/cmd
+
+echo "building firebench tool with Firecracker support"
+GOOS=linux go build -ldflags="-X main.EngineType=fc" -o bin/firebench -i github.com/dreadl0ck/firebench/cmd
