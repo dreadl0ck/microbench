3d552ffc8b7ee67990cf5055f3e4d950fdd6be7c
dreadl0ck <dreadl0ck@protonmail.ch>
2019-12-18 11:53:14 +0100

    updated build script

--- build.sh
+++ build.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-if [[ "$1" == "qemu" ]]; then
-  echo "building qemubench tool with QEMU support"
-  GOOS=linux go build -ldflags="-X main.EngineType=qemu" -o bin/qemubench -i github.com/dreadl0ck/firebench/cmd
-else
-  echo "building firebench tool with Firecracker support"
-  GOOS=linux go build -ldflags="-X main.EngineType=fc" -o bin/firebench -i github.com/dreadl0ck/firebench/cmd
-fi
+echo "building firebench tool"
+GOOS=linux go build -o bin/firebench -i github.com/dreadl0ck/firebench/cmd
