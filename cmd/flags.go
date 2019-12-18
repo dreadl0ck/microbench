98e35a6495fad11ab3fdb62d17d67348bbcfff66
dreadl0ck <dreadl0ck@protonmail.ch>
2019-12-18 23:24:01 +0100

    updated multi mode

--- cmd/flags.go
+++ cmd/flags.go
@@ -16,5 +16,7 @@ var (
 	flagEngineType = flag.String("e", "fc", "set engine type")
 	flagNumRepetitions = flag.Int("n", 1, "set number of repetitions")
 	flagVersion = flag.Bool("version", false, "print firebench version and exit")
+
+	flagNumVMs = flag.Int("numVMs", 10, "number of vms for multi mode")
 )
 
